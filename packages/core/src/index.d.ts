d784b4a43b53e188801e2fd731549aaa3019b2bd
lukitadproxd-netizen <lukitadproxd@gmail.com>
2026-07-16 22:45:47 -0300

    chore(repo): remove compiled artifacts accidentally tracked in src/
    19 .js/.d.ts/.js.map/.d.ts.map files were committed inside src/
    directories instead of dist/, likely from a tsc run with a misconfigured
    outDir at some point. Source .ts files are untouched. Confirmed the
    build regenerates dist/ correctly without these files present.
    
    Signed-off-by: lukitadproxd-netizen <lukitadproxd@gmail.com>

--- packages/core/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './types.js';
-export * from './validator.js';
-export * from './trajectory.js';
-//# sourceMappingURL=index.d.ts.map
--- packages/core/src/index.js
+++
@@ -1,4 +0,0 @@
-export * from './types.js';
-export * from './validator.js';
-export * from './trajectory.js';
-//# sourceMappingURL=index.js.map
